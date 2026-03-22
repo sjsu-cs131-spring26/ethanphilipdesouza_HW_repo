896ac8bfe6efc6328064e68d8abe15c96a2f3e78
Ethan De Souza <ethanphilipdesouza@gmail.com>
2026-03-21 20:32:54 -0700

    Update hw6_awk.sh

--- hw06/hw6_awk.sh
+++ hw06/hw6_awk.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
 set -eu
-
 DATA="/mnt/scratch/CS131_jelenag/amazon_reviews_full.tsv"
 OUT="out"
 TMP="$OUT/tmp"
-
 mkdir -p "$OUT" "$TMP"
+awk -F '\t' 'BEGIN{OFS="\t"}
+NR==1 {
+  print "review_id","product_id","product_category","star_rating","helpful_votes","total_votes"
+  next
+}
+NR<=1000 {
+  print $3,$4,$7,$8,$9,$10
+}' "$DATA" > "$OUT/task1.tsv"
 
 awk -F '\t' 'BEGIN{OFS="\t"}
-NR==1 {print "review_id","product_id","product_category","star_rating","helpful_votes","total_votes"; next}
-NR<=1000 {print $3,$4,$7,$8,$9,$10}
-' "$DATA" > "$OUT/task1.tsv"
-
-awk -F '\t' 'BEGIN{OFS="\t"}
-NR==1 {print "review_id","product_id","product_category","star_rating","helpful_votes","total_votes"; next}
+NR==1 {
+  print "review_id","product_id","product_category","star_rating","helpful_votes","total_votes"
+  next
+}
 {
   body=$14
   gsub(/^ +| +$/,"",body)
@@ -24,15 +28,16 @@ NR==1 {print "review_id","product_id","product_category","star_rating","helpful_
 
 awk -F '\t' 'BEGIN{OFS="\t"}
 NR>1 {
-  h=$9+0; t=$10+0
+  h=$9+0
+  t=$10+0
   if(t==0) b="NA"
   else {
     r=h/t
-    if(r==0) b="ZERO"
-    else if(r>=0.8) b="HI"
+    if(r>=0.8) b="HI"
     else if(r>=0.5) b="MID"
     else if(r>=0.1) b="LO"
-    else b="ZERO"
+    else if(r==0) b="ZERO"
+    else b="LO"
   }
   c[b]++
 }
@@ -41,10 +46,12 @@ END {
   for(b in c) print b,c[b]
 }' "$DATA" > "$TMP/t3.tsv"
 
-{ head -n1 "$TMP/t3.tsv"; tail -n+2 "$TMP/t3.tsv" | sort -k2,2nr; } > "$OUT/task3.tsv"
-
+{ head -n1 "$TMP/t3.tsv"; tail -n +2 "$TMP/t3.tsv" | sort -k2,2nr; } > "$OUT/task3.tsv"
 awk -F '\t' 'BEGIN{OFS="\t"}
-NR>1 {sum[$4]+=$8; cnt[$4]++}
+NR>1 {
+  sum[$4]+=$8
+  cnt[$4]++
+}
 END {
   print "product_id","count","avg_star_rating"
   for(p in cnt)
@@ -52,33 +59,36 @@ END {
       printf "%s\t%d\t%.2f\n",p,cnt[p],sum[p]/cnt[p]
 }' "$DATA" > "$TMP/t4.tsv"
 
-{ head -n1 "$TMP/t4.tsv"; tail -n+2 "$TMP/t4.tsv" | sort -k3,3nr -k2,2nr; } > "$OUT/task4.tsv"
+{ head -n1 "$TMP/t4.tsv"; tail -n +2 "$TMP/t4.tsv" | sort -k3,3nr -k2,2nr; } > "$OUT/task4.tsv"
 
 awk -F '\t' 'BEGIN{OFS="\t"}
 NR>1 && $12=="Y" {
-  c=$7; s=$8
-  k=c SUBSEP s
-  cnt[k]++; tot[c]++; cats[c]=1
+  c=$7
+  s=$8
+  cnt[c SUBSEP s]++
+  tot[c]++
+  cats[c]=1
 }
 END {
   print "product_category","star_1","star_2","star_3","star_4","star_5","total"
   for(c in cats)
     printf "%s\t%d\t%d\t%d\t%d\t%d\t%d\n",
-    c,
-    cnt[c SUBSEP 1]+0,
-    cnt[c SUBSEP 2]+0,
-    cnt[c SUBSEP 3]+0,
-    cnt[c SUBSEP 4]+0,
-    cnt[c SUBSEP 5]+0,
-    tot[c]
+      c,
+      cnt[c SUBSEP 1]+0,
+      cnt[c SUBSEP 2]+0,
+      cnt[c SUBSEP 3]+0,
+      cnt[c SUBSEP 4]+0,
+      cnt[c SUBSEP 5]+0,
+      tot[c]
 }' "$DATA" > "$TMP/t5.tsv"
 
-{ head -n1 "$TMP/t5.tsv"; tail -n+2 "$TMP/t5.tsv" | sort -k7,7nr; } > "$OUT/task5.tsv"
+{ head -n1 "$TMP/t5.tsv"; tail -n +2 "$TMP/t5.tsv" | sort -k7,7nr; } > "$OUT/task5.tsv"
 
 awk -F '\t' 'BEGIN{OFS="\t"}
 NR>1 {
   m=substr($15,1,7)
-  cnt[m]++; sum[m]+=$8
+  cnt[m]++
+  sum[m]+=$8
 }
 END {
   print "month","count","avg_star_rating"
@@ -86,7 +96,7 @@ END {
     printf "%s\t%d\t%.2f\n",m,cnt[m],sum[m]/cnt[m]
 }' "$DATA" > "$TMP/t6.tsv"
 
-{ head -n1 "$TMP/t6.tsv"; tail -n+2 "$TMP/t6.tsv" | sort; } > "$OUT/task6.tsv"
+{ head -n1 "$TMP/t6.tsv"; tail -n +2 "$TMP/t6.tsv" | sort -k1,1; } > "$OUT/task6.tsv"
 
 awk -F '\t' 'BEGIN{OFS="\t"}
 NR>1 && $12=="Y" {
@@ -101,7 +111,7 @@ END {
   for(k in c) print k,c[k]
 }' "$DATA" > "$TMP/t7.tsv"
 
-{ head -n1 "$TMP/t7.tsv"; tail -n+2 "$TMP/t7.tsv" | sort -k2,2nr; } > "$OUT/task7.tsv"
+{ head -n1 "$TMP/t7.tsv"; tail -n +2 "$TMP/t7.tsv" | sort -k2,2nr; } > "$OUT/task7.tsv"
 
 awk -F '\t' 'BEGIN{OFS="\t"}
 NR>1 {
@@ -111,17 +121,18 @@ NR>1 {
 END {
   print "customer_id","date","count"
   for(k in c)
-    if(c[k]>=5){
+    if(c[k]>=5) {
       split(k,a,SUBSEP)
       print a[1],a[2],c[k]
     }
 }' "$DATA" > "$TMP/t8.tsv"
+{ head -n1 "$TMP/t8.tsv"; tail -n +2 "$TMP/t8.tsv" | sort -k3,3nr -k2,2; } > "$OUT/task8.tsv"
 
-{ head -n1 "$TMP/t8.tsv"; tail -n+2 "$TMP/t8.tsv" | sort -k3,3nr; } > "$OUT/task8.tsv"
 
 awk -F '\t' 'BEGIN{OFS="\t"}
 NR>1 {
-  c=$7; all[c]++
+  c=$7
+  all[c]++
   if($12=="Y") v[c]++
 }
 END {
@@ -130,12 +141,16 @@ END {
     printf "%s\t%d\t%.1f\n",c,all[c],(v[c]/all[c])*100
 }' "$DATA" > "$TMP/t9.tsv"
 
-{ head -n1 "$TMP/t9.tsv"; tail -n+2 "$TMP/t9.tsv" | sort -k3,3nr; } > "$OUT/task9.tsv"
+{ head -n1 "$TMP/t9.tsv"; tail -n +2 "$TMP/t9.tsv" | sort -k3,3nr -k2,2nr; } > "$OUT/task9.tsv"
 
 awk -F '\t' 'BEGIN{OFS="\t"}
 NR>1 {
-  p=$4; all[p]++
-  if($10>0){sum[p]+=$9/$10; cnt[p]++}
+  p=$4
+  all[p]++
+  if($10>0) {
+    sum[p]+=$9/$10
+    cnt[p]++
+  }
 }
 END {
   print "product_id","count_all","avg_helpfulness_ratio"
@@ -143,9 +158,6 @@ END {
     if(all[p]>=100 && cnt[p]>0)
       printf "%s\t%d\t%.2f\n",p,all[p],sum[p]/cnt[p]
 }' "$DATA" > "$TMP/t10.tsv"
-
-{ head -n1 "$TMP/t10.tsv"; tail -n+2 "$TMP/t10.tsv" | sort -k3,3nr; } > "$OUT/task10.tsv"
-
+{ head -n1 "$TMP/t10.tsv"; tail -n +2 "$TMP/t10.tsv" | sort -k3,3nr -k2,2nr; } > "$OUT/task10.tsv"
 rm -rf "$TMP"
-
 echo "Done"
